5478dfebb3728ee276b7d3c89adfe1c09620c942
Alex Mattera <alex.mattera@yieldify.com>
2019-11-13 00:14:35 +0000

    add volume command

--- dist/app.js
+++ dist/app.js
@@ -5,13 +5,18 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const discord_js_1 = require("discord.js");
 const ytdl_core_1 = __importDefault(require("ytdl-core"));
-const TOKEN = 'NjM0Njg0NDg2NzY4MTMyMDk2.XanuVg.hDZkh8fH1jgrEPidyi_oaQgFQvU';
+const youtube_search_1 = __importDefault(require("youtube-search"));
+require('dotenv').config();
 const client = new discord_js_1.Client();
+const key = process.env.API_KEY;
 const COMMAND_PREFIX = '!';
 const COMMAND_JOIN = COMMAND_PREFIX + 'join';
 const COMMAND_PLAY = COMMAND_PREFIX + 'play';
 const COMMAND_PAUSE = COMMAND_PREFIX + 'pause';
 const COMMAND_STOP = COMMAND_PREFIX + 'stop';
+const COMMAND_VOLUME = COMMAND_PREFIX + 'volume';
+const COMMAND_STRING_SPLIT_LIMIT = 2;
+let dispatcher;
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}! Now even more powerful!`);
 });
@@ -25,33 +30,52 @@ client.on('message', (msg) => {
     // we ignore it
     if (!msg.guild)
         return;
-    const msgSubs = msg.content.split(' ');
     if (msg.content.startsWith(COMMAND_PLAY)) {
-        // Only try to join the sender's voice channel if they are in one themselves
-        if (msg.member.voiceChannel) {
-            msg.member.voiceChannel
-                .join()
-                .then((connection) => {
-                // Connection is an instance of VoiceConnection
-                msg.reply('I have successfully connected to the channel!');
-                if (msgSubs[1] != null) {
-                    ytdl_core_1.default.getInfo(msgSubs[1], (err, info) => {
-                        msg.reply(`Begin playing: "${info.title}" Duration: ${info.timestamp}`);
-                        if (err) {
-                            msg.reply(`Error ${err}`);
-                        }
-                    });
-                    connection.playStream(ytdl_core_1.default(msgSubs[1]));
-                }
-                else
-                    msg.reply('Please give something for me to play.');
-            })
-                .catch(console.log);
+        const match = msg.content.match(new RegExp(`(${COMMAND_PLAY})(.*)`));
+        if (match.length > 0) {
+            const query = match[2];
+            // Only try to join the sender's voice channel if they are in one themselves
+            if (msg.member.voiceChannel) {
+                msg.member.voiceChannel.join().then((connection) => {
+                    if (query != null) {
+                        youtube_search_1.default(query, { maxResults: 1, key: key }, (err, results) => {
+                            if (err) {
+                                msg.reply(err.message);
+                                return;
+                            }
+                            const video = results[0];
+                            msg.reply(`Trying to play "${video.title}"`);
+                            dispatcher = connection.playStream(ytdl_core_1.default(video.link, { quality: 'highestaudio' }));
+                            dispatcher.setVolume(0.5);
+                            dispatcher.on('start', () => msg.channel.send(`Now Playing: ${video.title}`));
+                            connection.on('disconnect', () => dispatcher.end());
+                        });
+                    }
+                    else
+                        msg.reply('Please give something for me to play.');
+                });
+            }
+            else {
+                msg.reply('You need to join a voice channel first!');
+            }
         }
-        else {
-            msg.reply('You need to join a voice channel first!');
+    }
+    if (msg.content === COMMAND_PAUSE) {
+        msg.reply('Playback paused.');
+        dispatcher.pause();
+    }
+    if (msg.content === COMMAND_STOP) {
+        msg.reply('Playback stopped.');
+        dispatcher.end();
+    }
+    if (msg.content === COMMAND_VOLUME) {
+        const match = msg.content.match(new RegExp(`(${COMMAND_VOLUME}) ([-.0-9]+)`));
+        if (match.length > 0) {
+            const volume = parseFloat(match[2]);
+            msg.reply(`Setting volume to ${volume}`);
+            dispatcher.setVolume(volume);
         }
     }
 });
-client.login(TOKEN);
+client.login(process.env.TOKEN);
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,21 @@
-import { Client } from 'discord.js';
+import { Client, DiscordAPIError, StreamDispatcher } from 'discord.js';
 import ytdl from 'ytdl-core';
+import search from 'youtube-search';
 
 require('dotenv').config();
 
 const client = new Client();
+const key = process.env.API_KEY;
 
 const COMMAND_PREFIX = '!';
 const COMMAND_JOIN = COMMAND_PREFIX + 'join';
 const COMMAND_PLAY = COMMAND_PREFIX + 'play';
 const COMMAND_PAUSE = COMMAND_PREFIX + 'pause';
 const COMMAND_STOP = COMMAND_PREFIX + 'stop';
+const COMMAND_VOLUME = COMMAND_PREFIX + 'volume';
+const COMMAND_STRING_SPLIT_LIMIT = 2;
+
+let dispatcher: StreamDispatcher;
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}! Now even more powerful!`);
@@ -26,31 +32,52 @@ client.on('message', (msg) => {
   // we ignore it
   if (!msg.guild) return;
 
-  const msgSubs = msg.content.split(' ');
-
   if (msg.content.startsWith(COMMAND_PLAY)) {
-    // Only try to join the sender's voice channel if they are in one themselves
-    if (msg.member.voiceChannel) {
-      msg.member.voiceChannel
-        .join()
-        .then((connection) => {
-          // Connection is an instance of VoiceConnection
-          msg.reply('I have successfully connected to the channel!');
-          if (msgSubs[1] != null) {
-            ytdl.getInfo(msgSubs[1], (err, info) => {
-              msg.reply(`Begin playing: "${info.title}"`);
+    const match = msg.content.match(new RegExp(`(${COMMAND_PLAY})(.*)`));
+    if (match.length > 0) {
+      const query = match[2];
+
+      // Only try to join the sender's voice channel if they are in one themselves
+      if (msg.member.voiceChannel) {
+        msg.member.voiceChannel.join().then((connection) => {
+          if (query != null) {
+            search(query, { maxResults: 1, key: key }, (err, results) => {
               if (err) {
-                msg.reply(`Error ${err}`);
+                msg.reply(err.message);
+                return;
               }
+              const video = results[0];
+              msg.reply(`Trying to play "${video.title}"`);
+              dispatcher = connection.playStream(ytdl(video.link, { quality: 'highestaudio' }));
+              dispatcher.setVolume(0.5);
+              dispatcher.on('start', () => msg.channel.send(`Now Playing: ${video.title}`));
+
+              connection.on('disconnect', () => dispatcher.end());
             });
-            connection.playStream(ytdl(msgSubs[1]));
           } else msg.reply('Please give something for me to play.');
-        })
-        .catch(console.log);
-    } else {
-      msg.reply('You need to join a voice channel first!');
+        });
+      } else {
+        msg.reply('You need to join a voice channel first!');
+      }
+    }
+  }
+  if (msg.content === COMMAND_PAUSE) {
+    msg.reply('Playback paused.');
+    dispatcher.pause();
+  }
+
+  if (msg.content === COMMAND_STOP) {
+    msg.reply('Playback stopped.');
+    dispatcher.end();
+  }
+
+  if (msg.content === COMMAND_VOLUME) {
+    const match = msg.content.match(new RegExp(`(${COMMAND_VOLUME}) ([-.0-9]+)`));
+    if (match.length > 0) {
+      const volume = parseFloat(match[2]);
+      msg.reply(`Setting volume to ${volume}`);
+      dispatcher.setVolume(volume);
     }
   }
 });
-
 client.login(process.env.TOKEN);
