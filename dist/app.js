a11916cf6556f95f6845146d1efa75439e18fb06
Alex Mattera <alexmattera@outlook.com>
2019-11-28 02:57:32 +0000

    implement ytdl-core-discord (#13)
    * issue(1): move secrets out of repo
    
    * v0.1 release (#8)
    
    * add promise error handling (#10)
    
    * Implement ytdl-core-discord module (#12)
    
    * add promise error handling
    
    * implement ytdl-core-discord module
    
    * remove /dist files

--- dist/app.js
+++ dist/app.js
@@ -1,22 +1,30 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const discord_js_1 = require("discord.js");
-const ytdl_core_1 = __importDefault(require("ytdl-core"));
+const ytdl_core_discord_1 = __importDefault(require("ytdl-core-discord"));
 const youtube_search_1 = __importDefault(require("youtube-search"));
 require('dotenv').config();
 const client = new discord_js_1.Client();
-const key = process.env.API_KEY;
 const COMMAND_PREFIX = '!';
-const COMMAND_JOIN = COMMAND_PREFIX + 'join';
+// const COMMAND_JOIN = COMMAND_PREFIX + 'join';
 const COMMAND_PLAY = COMMAND_PREFIX + 'play';
 const COMMAND_PAUSE = COMMAND_PREFIX + 'pause';
 const COMMAND_STOP = COMMAND_PREFIX + 'stop';
 const COMMAND_VOLUME = COMMAND_PREFIX + 'volume';
-const COMMAND_STRING_SPLIT_LIMIT = 2;
 let dispatcher;
+process.on('unhandledRejection', console.error);
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}! Now even more powerful!`);
 });
@@ -32,28 +40,37 @@ client.on('message', (msg) => {
         return;
     if (msg.content.startsWith(COMMAND_PLAY)) {
         const match = msg.content.match(new RegExp(`(${COMMAND_PLAY})(.*)`));
-        if (match.length > 0) {
+        if (match !== null && match.length > 0) {
             const query = match[2];
             // Only try to join the sender's voice channel if they are in one themselves
             if (msg.member.voiceChannel) {
-                msg.member.voiceChannel.join().then((connection) => {
+                msg.member.voiceChannel
+                    .join()
+                    .then((connection) => {
                     if (query != null) {
-                        youtube_search_1.default(query, { maxResults: 1, key: key }, (err, results) => {
+                        youtube_search_1.default(query, { maxResults: 1, key: process.env.YOUTUBE_API_KEY }, (err, results) => __awaiter(void 0, void 0, void 0, function* () {
                             if (err) {
                                 msg.reply(err.message);
                                 return;
                             }
+                            if (results === undefined) {
+                                msg.reply('No matching results found');
+                                return;
+                            }
                             const video = results[0];
-                            msg.reply(`Trying to play "${video.title}"`);
-                            dispatcher = connection.playStream(ytdl_core_1.default(video.link, { quality: 'highestaudio' }));
+                            msg.reply(`Searching for "${video.title}"`);
+                            dispatcher = connection.playOpusStream(yield ytdl_core_discord_1.default(video.link));
                             dispatcher.setVolume(0.5);
                             dispatcher.on('start', () => msg.channel.send(`Now Playing: ${video.title}`));
+                            dispatcher.on('end', () => msg.channel.send('Finished playing'));
+                            dispatcher.on('error', (e) => console.log('Error: ', e));
                             connection.on('disconnect', () => dispatcher.end());
-                        });
+                        }));
                     }
                     else
                         msg.reply('Please give something for me to play.');
-                });
+                })
+                    .catch((e) => msg.reply('Error: ' + e));
             }
             else {
                 msg.reply('You need to join a voice channel first!');
@@ -70,12 +87,12 @@ client.on('message', (msg) => {
     }
     if (msg.content === COMMAND_VOLUME) {
         const match = msg.content.match(new RegExp(`(${COMMAND_VOLUME}) ([-.0-9]+)`));
-        if (match.length > 0) {
+        if (match !== null && match.length > 0) {
             const volume = parseFloat(match[2]);
             msg.reply(`Setting volume to ${volume}`);
             dispatcher.setVolume(volume);
         }
     }
 });
-client.login(process.env.TOKEN);
+client.login(process.env.DISCORD_BOT_TOKEN);
 //# sourceMappingURL=app.js.map
